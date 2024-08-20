8b672c29d71982a83cde79f09f8abdc94ec9e8a8
Ara Hacopian <ara@hacopian.de>
2024-08-20 17:24:43 +0200

    Add good_job

---
+++ config/initializers/good_job.rb
@@ -0,0 +1,12 @@
+Rails.application.configure do
+  config.good_job = {
+    preserve_job_records: true,
+    retry_on_unhandled_error: false,
+    on_thread_error: ->(exception) { Rails.error.report(exception) },
+    execution_mode: :async,
+    queues: "*",
+    max_threads: 5,
+    poll_interval: 30,
+    shutdown_timeout: 25
+  }
+end
